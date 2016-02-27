6af2f5274b177d6ed4d903341997efb0efc65f62
Cooper Pellaton <c@cepp.at>
2016-02-26 23:39:48 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,35 @@
+var five = require("johnny-five");
+var Edison = require("galileo-io");
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname));
+
+server.listen(80);
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+
+var board = new five.Board({
+    io: new Edison()
+});
+
+board.on("ready", function() {
+    var sensor = new five.Sensor({
+        pin: "A1",
+        freq:25,
+        threshold: 100
+    });
+    sensor.on("change", function() {
+        console.log(this.value);
+        io.emit('news');
+        });
+});
+
+    // sensor.within([500, 1500], function() {
+    //     console.log(this.value);
+    // })
